f5182064595656635bf692307bc0226ed2ab4056
Cody Romano <codyromano@gmail.com>
2024-05-19 15:30:18 -0700

    sentry integration

---
+++ src/configureSentry.ts
@@ -0,0 +1,16 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://bf0d0d77bcfe40a6e1e800b46901c730@o4507285418934272.ingest.us.sentry.io/4507285422080000",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/housefashionista\.com/],
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import "./configureSentry";
 import React, { Suspense, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import {
